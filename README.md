7b5542000f721ee46c98d6feb35110eadf800442
Art <artallo@mail.ru>
2020-10-28 18:40:19 +0400

    Create empty project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bangle programme
+
+Starts a FreeRTOS tasks to manage Bangle menu navigation 
+
+Proceesing button pressing and show current mode on LCD.
---
+++ main/bangle.c
@@ -0,0 +1,37 @@
+/**
+ * @file bangle.c
+ * @author Artem (artallo@mail.ru)
+ * @brief 
+ * @version 0.1
+ * @date 2020-10-28
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#define BUTTON_GPIO 27
+
+void app_main(void)
+{
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_pad_select_gpio(BUTTON_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+    while(1) {
+        printf("Checking button state: ");
+        printf("%d\n", gpio_get_level(BUTTON_GPIO));
+        for (int i = 2; i >= 0; i--) {
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    }
+}
