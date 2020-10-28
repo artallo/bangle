b52526a8348dbdb85284c42aef754f2e8d8f1a3c
Artem <artallo@mail.ru>
2020-10-29 00:17:41 +0400

    vTaskButtonCheck  work in progress

--- main/bangle.c
+++ main/bangle.c
@@ -16,6 +16,8 @@
 
 #define BUTTON_GPIO 27
 
+void vTaskButtonCheck(void *pvParameters);
+
 void app_main(void)
 {
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
@@ -25,13 +27,26 @@ void app_main(void)
        functions.)
     */
     gpio_pad_select_gpio(BUTTON_GPIO);
-    /* Set the GPIO as a push/pull output */
+    /* Set the GPIO as input */
     gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+    xTaskCreate(vTaskButtonCheck, "ButtonCheck", 32, NULL, 1, NULL);
+    vTaskStartScheduler();
     while(1) {
         printf("Checking button state: ");
         printf("%d\n", gpio_get_level(BUTTON_GPIO));
         for (int i = 2; i >= 0; i--) {
         vTaskDelay(200 / portTICK_PERIOD_MS);
     }
+    }
+}
+
+
+void vTaskButtonCheck(void *pvParameters) {
+    uint8_t duration = 0;
+    //see realization how measure key press duration in project SD reader/checker for Nikolay domofon
+    while(1) {
+        if (gpio_get_level(BUTTON_GPIO) == 0) {
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+        }
     }
 }
