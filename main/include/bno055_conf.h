647c37bba6a36d366b4ff455ce6ccff150212f11
Art <artallo@mail.ru>
2020-12-08 17:44:04 +0400

    bno055_conf added

---
+++ main/include/bno055_conf.h
@@ -0,0 +1,13 @@
+/**
+ * @file bno055_conf.h
+ * @author Artem
+ * @brief Configuration file for the SSD1306 library.
+ * @version 0.1
+ * @date 2020-12-08
+ */
+#ifndef __BNO055_CONF_H__
+#define __BNO055_CONF_H__
+
+#define BNO055_I2C_PORT I2C_NUM_1 ///< I2C port number. There are 2 i2c port in esp32: I2C_NUM_0 and I2C_NUM_1
+
+#endif /* __BNO055_CONF_H__ */
